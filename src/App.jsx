82a6836a5f82068d9d7a926ac64897d8ae8e0a42
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-06-30 18:40:48 +0600

    creat fake data for the blogs and load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Blogs from './Components/Header/Blogs/Blogs'
+import Header from './Components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
