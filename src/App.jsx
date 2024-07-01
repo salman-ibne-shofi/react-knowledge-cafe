4a16c133a94d9d4e0e95e235899121bd04847fba
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-07-01 16:50:16 +0600

    add time when marked as read

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import Bookmarks from './Components/Bookmarks/Bookmarks'
-import Blogs from './Components/Header/Blogs/Blogs'
-import Header from './Components/Header/Header'
+import { useState } from "react";
+import "./App.css";
+import Bookmarks from "./Components/Bookmarks/Bookmarks";
+import Blogs from "./Components/Header/Blogs/Blogs";
+import Header from "./Components/Header/Header";
 
 function App() {
-   const [bookmarks, setBookmarks] = useState([]);
+	const [bookmarks, setBookmarks] = useState([]);
+	const [readingTime, setReadingTime] = useState(0);
 
-    const handleAddToBookmark = blog =>{
-        const newBookmarks = [...bookmarks, blog];
-        setBookmarks(newBookmarks);
-    }
+	const handleAddToBookmark = (blog) => {
+		const newBookmarks = [...bookmarks, blog];
+		setBookmarks(newBookmarks);
+	};
 
-  return (
-    <>
-      <Header></Header>
-      <main className='md:flex max-w-7xl mx-auto'>
-      <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
-      <Bookmarks bookmarks={bookmarks}></Bookmarks>
-      </main>
-    </>
-  )
+	const handleMarkAsRead = (time) => {
+		const newReadingTime = readingTime + time;
+		setReadingTime(newReadingTime);
+	};
+
+	return (
+		<>
+			<Header></Header>
+			<main className="md:flex max-w-7xl mx-auto">
+				<Blogs
+					handleAddToBookmark={handleAddToBookmark}
+					handleMarkAsRead={handleMarkAsRead}
+				></Blogs>
+				<Bookmarks
+					bookmarks={bookmarks}
+					readingTime={readingTime}
+				></Bookmarks>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { FaBookmark } from "react-icons/fa";
 
-const Blog = ({ blog, handleAddToBookmark }) => {
+const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
 	const {
 		title,
 		cover,
@@ -12,7 +12,7 @@ const Blog = ({ blog, handleAddToBookmark }) => {
 		hashtags,
 	} = blog;
 	return (
-		<div className="mb-20">
+		<div className="mb-20 space-y-4">
 			<img
 				className="mb-8"
 				src={cover}
@@ -45,13 +45,15 @@ const Blog = ({ blog, handleAddToBookmark }) => {
 					</span>
 				))}
 			</p>
+            <button onClick={() => handleMarkAsRead(reading_time)} className='text-purple-800 font-bold underline'>Mark As Read</button>
 		</div>
 	);
 };
 
 Blog.propTypes = {
 	blog: PropTypes.object.isRequired,
-    handleAddToBookmark: PropTypes.func
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
 
 };
 
